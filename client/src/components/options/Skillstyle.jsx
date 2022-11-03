66f9c20032d5b61f6bebb32e133441cb5b789144
최정훈/Hamoonrang <hamoonrang@gmail.com>
2022-11-03 17:16:29 +0900

    dw

--- client/src/components/options/Skillstyle.jsx
+++ client/src/components/options/Skillstyle.jsx
@@ -4,25 +4,30 @@ function Skillstyle(props) {
     const active_Skill = props.active_Skill;
     const passive_Skill = props.passive_Skill;
   return (
-    <div>
-        <div>
-            {active_Skill.length > 0 ?  
-            active_Skill.map((data,index)=>(
-                <div key={index}>
-                    {data.name} / {data.level}
-                </div>
-            ))
-            :""}
+    <div className='skills'>
+        <div className='activeSkill'>
+            <h3>액티브</h3>
+            <div>
+                {active_Skill.length > 0 ?  
+                active_Skill.map((data,index)=>(
+                    <div key={index}>
+                        {data.name} / {data.level}
+                    </div>
+                ))
+                :""}
+            </div>
         </div>
-        <div>
-            {passive_Skill.length > 0 ?  
-            passive_Skill.map((data,index)=>(
-                <div key={index}>
-                    {data.name} / {data.level}
-                </div>
-            ))
-            :""}
-
+        <div className='passiveSkill'>
+            <h3>패시브</h3>
+            <div>
+                {passive_Skill.length > 0 ?  
+                passive_Skill.map((data,index)=>(
+                    <div key={index}>
+                        {data.name} / {data.level}
+                    </div>
+                ))
+                :""}
+            </div>
         </div>
     </div>
   )
