735e40367c3e8336ec663707360b8cf4e6542ffc
최정훈/Hamoonrang <hamoonrang@gmail.com>
2022-11-14 18:07:10 +0900

    fs

--- client/src/components/page/ItemSises.jsx
+++ client/src/components/page/ItemSises.jsx
@@ -7,15 +7,15 @@ function ItemSises() {
       <div className='auctionSise'>
          {itemSiseList.length > 0 ? 
          itemSiseList.map((data,index)=>(
-            <div key={index}>
+            <div className='yobm owdsc cccs' key={index}>
                <div>
                   <span>시세 기준: {(data.soldDate).slice(5,16)}</span>
                   <img src={`https://img-api.neople.co.kr/df/items/${data.itemId}`} alt={data.itemName} />
                </div>
                <div>
-
-               </div>
+               {data.itemName}
                {(data.unitPrice).toLocaleString("ko-KR")}
+               </div>
             </div>
          ))
          :"b"}
--- server/routes/items.js
+++ server/routes/items.js
@@ -3,12 +3,24 @@ const router = express.Router();
 const axios = require("axios");
 const neoplrAPI = require("../configs/configs");
 
+const fillters = (elements) => {
+   if (elements.itemTypeDetail !== "소모품") {
+         return true;
+      } 
+}
+
 router.get("/", async (req, res, next) => {
    let itemName = req.query;
    let enCode = encodeURI(itemName.itemNamed);
+   let newObj = {}
    try {
-      await axios.get(`${neoplrAPI.firstURL}/auction-sold?itemName=${enCode}&wordType=front&wordShort=true&limit=20&apikey=${neoplrAPI.apiKey}`)
-         .then((response) => {res.send(response.data)})
+      await axios.get(`${neoplrAPI.firstURL}/auction-sold?itemName=${enCode}&wordType=full&wordShort=true&limit=50&apikey=${neoplrAPI.apiKey}`)
+         .then((response) => {
+            let datas = response.data.rows;
+            let filterArr = datas.filter(fillters);
+            newObj.rows = filterArr;
+            res.send(newObj);
+         })
    } catch (error) {
       next(error)
    }
